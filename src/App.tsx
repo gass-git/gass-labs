bf2ec22a14140032b573cea5a6c6f25dc5ecc3b4
Gabriel Salinas Szada <75760163+gass-git@users.noreply.github.com>
2026-08-12 12:16:49 +0200

    feat: installs React and adds a basic layout structure for the site

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="row navbar">
+        <div className="col brand">logo and brand name</div>
+        <div></div>
+        <div className="col menu">menu</div>
+      </div>
+
+      <div className="row top-image">TOP IMAGE</div>
+
+      <div className="row content">CONTENT</div>
+      <div className="row footer">FOOTER</div>
+    </>
+  );
+}
+
+export default App;
