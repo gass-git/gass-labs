f61bb9723ce40bd43d0bdc85185ce6b4aafb13cb
Gabriel Salinas Szada <75760163+gass-git@users.noreply.github.com>
2026-08-13 16:01:25 +0200

    wip: adds a rough Games section

--- src/sections/News.tsx
+++ src/sections/News.tsx
@@ -1,5 +1,5 @@
-import S from "./News.module.css"
-import img from "../assets/news-img-sample.png"
+import S from "./News.module.css";
+import img from "../assets/news-img-sample.png";
 
 export default function News() {
   return (
@@ -7,17 +7,14 @@ export default function News() {
       <Item />
       <Item />
     </div>
-  )
+  );
 }
 
-
-function Item(){
+function Item() {
   return (
     <div className={S.itemContainer}>
       <div className={S.col}>
-        <div className={S.title}>
-          New site is now live !
-        </div>
+        <div className={S.title}>New site is now live !</div>
       </div>
 
       <div className={S.col}>
@@ -25,10 +22,15 @@ function Item(){
           <img src={img} />
         </div>
         <div className={S.textWrapper}>
-          13 July 2026 – The summer update, now available for free, presents the Tibia community with one of its greatest challenges to date: The toughest bosses in the game’s 29-year history await players at the end of a new quest. True to an MMORPG with no level cap, their difficulty can be ramped up infinitely. This means that the bosses not only gain more health points, but the battles also become more challenging through additional game mechanics.
+          13 July 2026 - The summer update, now available for free, presents the
+          Tibia community with one of its greatest challenges to date: The
+          toughest bosses in the game's 29-year history await players at the end
+          of a new quest. True to an MMORPG with no level cap, their difficulty
+          can be ramped up infinitely. This means that the bosses not only gain
+          more health points, but the battles also become more challenging
+          through additional game mechanics.
         </div>
       </div>
-
     </div>
-  )
+  );
 }
