b0b31435a9cf301a6882cea3e6f70c563f1d359a
gass-git <75760163+gass-git@users.noreply.github.com>
2026-09-25 10:18:52 +0200

    feat: rough first implementation of the animated flask

--- src/components/Brand.tsx
+++ src/components/Brand.tsx
@@ -1,4 +1,4 @@
-import lab_flask from "../assets/lab_flask.png";
+import lab_flask from "../assets/flask_animation_v2.apng";
 import S from "./styles/Brand.module.css";
 
 export default function Brand() {
